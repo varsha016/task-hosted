118e5c389a7ce9447c1d56c76231d07e104cb350
varsha016 <103167478+varsha016@users.noreply.github.com>
2023-06-05 15:37:46 +0530

    products

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
             "http://localhost:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            // "https://easy-pear-bunny-wig.cyclic.app/"
+            "https://rich-jade-salmon-wrap.cyclic.app/"
         ]
         if (allowed.indexOf(o) !== -1 || !o) {
             cb(null, true)
