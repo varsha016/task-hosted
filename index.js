dcd7c447ac5e638afe0adbd0b4aee29df17f8112
varsha016 <103167478+varsha016@users.noreply.github.com>
2023-06-06 13:37:34 +0530

    initial data

--- index.js
+++ index.js
@@ -25,9 +25,8 @@ app.use(cors({
             "http://localhost:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            // "https://rich-jade-salmon-wrap.cyclic.app"
-            // "https://e-commerce-wmca.onrender.com"
-            "https://task-hosted.vercel.app"
+            "https://e-commerce-wmca.onrender.com"
+            // "https://task-hosted.vercel.app"
         ]
         if (allowed.indexOf(o) !== -1 || !o) {
             cb(null, true)
